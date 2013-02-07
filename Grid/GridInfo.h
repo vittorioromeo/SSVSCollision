3d70bdf45c9167c90006dd27759e84e6474b9e44
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-02-07 18:03:01 +0100

    License

--- Grid/GridInfo.h
+++ Grid/GridInfo.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2013 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+
 #ifndef GRIDINFO_H
 #define GRIDINFO_H
 
