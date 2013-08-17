829716958b16e3b5352d8cb1f03aac9eb5661b4d
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-17 18:15:33 +0200

    Refactoring, added default move ctor/assign

--- include/SSVSCollision/AABB/AABB.h
+++ include/SSVSCollision/AABB/AABB.h
@@ -16,8 +16,15 @@ namespace ssvsc
 			Vec2i position, halfSize;
 
 		public:
+			inline AABB(const AABB& mAABB) = default;
+			inline AABB(AABB&& mAABB) = default;
 			inline AABB(Vec2i mPosition, Vec2i mHalfSize) : position{mPosition}, halfSize{mHalfSize} { }
 			inline AABB(int mLeft, int mRight, int mTop, int mBottom) : position{mLeft + (mRight - mLeft) / 2, mTop + (mBottom - mTop) / 2}, halfSize{(mRight - mLeft) / 2, (mBottom - mTop) / 2} { }
+
+			inline AABB& operator=(const AABB& mAABB)			= default;
+			inline AABB& operator=(AABB&& mAABB)				= default;
+			inline bool operator==(const AABB& mOther) const	{ return position == mOther.position && halfSize == mOther.halfSize; }
+			inline bool operator!=(const AABB& mOther) const	{ return position != mOther.position || halfSize != mOther.halfSize; }
 
 			inline void move(Vec2i mOffset) { position += mOffset; }
 
@@ -43,26 +50,23 @@ namespace ssvsc
 			inline int getWidth() const			{ return halfSize.x * 2; }
 			inline int getHeight() const		{ return halfSize.y * 2; }
 
-			template<typename T> inline Vec2<T> getNWCorner() const			{ return Vec2<T>(getLeft(), getTop()); }
-			template<typename T> inline Vec2<T> getNECorner() const			{ return Vec2<T>(getRight(), getTop()); }
-			template<typename T> inline Vec2<T> getSWCorner() const			{ return Vec2<T>(getLeft(), getBottom()); }
-			template<typename T> inline Vec2<T> getSECorner() const			{ return Vec2<T>(getRight(), getBottom()); }
-			template<typename T> inline Segment<T> getLeftSegment() const	{ return {getNWCorner<T>(), getSWCorner<T>()}; }
-			template<typename T> inline Segment<T> getRightSegment() const	{ return {getNECorner<T>(), getSECorner<T>()}; }
-			template<typename T> inline Segment<T> getTopSegment() const	{ return {getNWCorner<T>(), getNECorner<T>()}; }
-			template<typename T> inline Segment<T> getBottomSegment() const	{ return {getSWCorner<T>(), getSECorner<T>()}; }
+			template<typename T> inline Vec2<T> getVertexNW() const			{ return Vec2<T>(getLeft(), getTop()); }
+			template<typename T> inline Vec2<T> getVertexNE() const			{ return Vec2<T>(getRight(), getTop()); }
+			template<typename T> inline Vec2<T> getVertexSW() const			{ return Vec2<T>(getLeft(), getBottom()); }
+			template<typename T> inline Vec2<T> getVertexSE() const			{ return Vec2<T>(getRight(), getBottom()); }
+			template<typename T> inline Segment<T> getSegmentLeft() const	{ return {getVertexNW<T>(), getVertexSW<T>()}; }
+			template<typename T> inline Segment<T> getSegmentRight() const	{ return {getVertexNE<T>(), getVertexSE<T>()}; }
+			template<typename T> inline Segment<T> getSegmentTop() const	{ return {getVertexNW<T>(), getVertexNE<T>()}; }
+			template<typename T> inline Segment<T> getSegmentBottom() const	{ return {getVertexSW<T>(), getVertexSE<T>()}; }
 
 			inline bool isLeftOf(const AABB& mAABB) const	{ return getRight() <= mAABB.getLeft(); }
 			inline bool isRightOf(const AABB& mAABB) const	{ return getLeft() >= mAABB.getRight(); }
 			inline bool isAbove(const AABB& mAABB) const	{ return getBottom() <= mAABB.getTop(); }
 			inline bool isBelow(const AABB& mAABB) const	{ return getTop() >= mAABB.getBottom(); }
 
-			inline bool operator==(const AABB& mOther) const { return position == mOther.position && halfSize == mOther.halfSize; }
-			inline bool operator!=(const AABB& mOther) const { return position != mOther.position || halfSize != mOther.halfSize; }
-
 			inline bool isOverlapping(const AABB& mAABB) const	{ return getRight() > mAABB.getLeft() && getLeft() < mAABB.getRight() && (getBottom() > mAABB.getTop() && getTop() < mAABB.getBottom()); }
-			inline bool contains(const AABB& mAABB) const		{ return !(mAABB.getLeft() < getLeft() || mAABB.getRight() >= getRight() || mAABB.getTop() < getTop() || mAABB.getBottom() >= getBottom()); }
-			inline bool contains(const Vec2i mPoint) const		{ return !(mPoint.x < getLeft() || mPoint.x >= getRight() || mPoint.y < getTop() || mPoint.y >= getBottom()); }
+			inline bool contains(const AABB& mAABB) const		{ return mAABB.getLeft() >= getLeft() && mAABB.getRight() < getRight() && mAABB.getTop() >= getTop() && mAABB.getBottom() < getBottom(); }
+			inline bool contains(const Vec2i mPoint) const		{ return mPoint.x >= getLeft() && mPoint.x < getRight() && mPoint.y >= getTop() && mPoint.y < getBottom(); }
 	};
 }
 
