92b5ed8091e3d0564e5efc3948bfbcf30daea789
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-28 12:41:11 +0200

    Pass vecs as const&

--- include/SSVSCollision/AABB/AABB.h
+++ include/SSVSCollision/AABB/AABB.h
@@ -18,7 +18,7 @@ namespace ssvsc
 		public:
 			inline AABB(const AABB& mAABB) = default;
 			inline AABB(AABB&& mAABB) = default;
-			inline AABB(Vec2i mPosition, Vec2i mHalfSize) : position{mPosition}, halfSize{mHalfSize} { }
+			inline AABB(const Vec2i& mPosition, const Vec2i& mHalfSize) : position{mPosition}, halfSize{mHalfSize} { }
 			inline AABB(int mLeft, int mRight, int mTop, int mBottom) : position{mLeft + (mRight - mLeft) / 2, mTop + (mBottom - mTop) / 2}, halfSize{(mRight - mLeft) / 2, (mBottom - mTop) / 2} { }
 
 			inline AABB& operator=(const AABB& mAABB)			= default;
@@ -26,29 +26,29 @@ namespace ssvsc
 			inline bool operator==(const AABB& mOther) const	{ return position == mOther.position && halfSize == mOther.halfSize; }
 			inline bool operator!=(const AABB& mOther) const	{ return !(*this == mOther); }
 
-			inline void move(Vec2i mOffset) { position += mOffset; }
+			inline void move(const Vec2i& mOffset) { position += mOffset; }
 
-			inline void setPosition(Vec2i mPosition)	{ position = mPosition; }
-			inline void setX(int mX)					{ position.x = mX; }
-			inline void setY(int mY)					{ position.y = mY; }
-			inline void setHalfSize(Vec2i mHalfSize)	{ halfSize = mHalfSize; }
-			inline void setSize(Vec2i mSize)			{ halfSize = mSize / 2; }
-			inline void setWidth(int mWidth)			{ halfSize.x = mWidth / 2; }
-			inline void setHeight(int mHeight)			{ halfSize.y = mHeight / 2; }
+			inline void setPosition(const Vec2i& mPosition)	{ position = mPosition; }
+			inline void setX(int mX)						{ position.x = mX; }
+			inline void setY(int mY)						{ position.y = mY; }
+			inline void setHalfSize(const Vec2i& mHalfSize)	{ halfSize = mHalfSize; }
+			inline void setSize(const Vec2i& mSize)			{ halfSize = mSize / 2; }
+			inline void setWidth(int mWidth)				{ halfSize.x = mWidth / 2; }
+			inline void setHeight(int mHeight)				{ halfSize.y = mHeight / 2; }
 
-			inline Vec2i getPosition() const	{ return position; }
-			inline int getX() const				{ return position.x; }
-			inline int getY() const				{ return position.y; }
-			inline int getLeft() const			{ return position.x - halfSize.x; }
-			inline int getRight() const			{ return position.x + halfSize.x; }
-			inline int getTop() const			{ return position.y - halfSize.y; }
-			inline int getBottom() const		{ return position.y + halfSize.y; }
-			inline Vec2i getHalfSize() const	{ return halfSize; }
-			inline int getHalfWidth() const		{ return halfSize.x; }
-			inline int getHalfHeight() const	{ return halfSize.y; }
-			inline Vec2i getSize() const		{ return halfSize * 2; }
-			inline int getWidth() const			{ return halfSize.x * 2; }
-			inline int getHeight() const		{ return halfSize.y * 2; }
+			inline const Vec2i& getPosition() const	{ return position; }
+			inline int getX() const					{ return position.x; }
+			inline int getY() const					{ return position.y; }
+			inline int getLeft() const				{ return position.x - halfSize.x; }
+			inline int getRight() const				{ return position.x + halfSize.x; }
+			inline int getTop() const				{ return position.y - halfSize.y; }
+			inline int getBottom() const			{ return position.y + halfSize.y; }
+			inline const Vec2i& getHalfSize() const	{ return halfSize; }
+			inline int getHalfWidth() const			{ return halfSize.x; }
+			inline int getHalfHeight() const		{ return halfSize.y; }
+			inline const Vec2i& getSize() const		{ return halfSize * 2; }
+			inline int getWidth() const				{ return halfSize.x * 2; }
+			inline int getHeight() const			{ return halfSize.y * 2; }
 
 			template<typename T> inline Vec2<T> getVertexNW() const			{ return Vec2<T>(getLeft(), getTop()); }
 			template<typename T> inline Vec2<T> getVertexNE() const			{ return Vec2<T>(getRight(), getTop()); }
@@ -66,7 +66,7 @@ namespace ssvsc
 
 			inline bool isOverlapping(const AABB& mAABB) const	{ return !isLeftOf(mAABB) && !isRightOf(mAABB) && !isAbove(mAABB) && !isBelow(mAABB); }
 			inline bool contains(const AABB& mAABB) const		{ return mAABB.getLeft() >= getLeft() && mAABB.getRight() < getRight() && mAABB.getTop() >= getTop() && mAABB.getBottom() < getBottom(); }
-			inline bool contains(Vec2i mPoint) const			{ return mPoint.x >= getLeft() && mPoint.x < getRight() && mPoint.y >= getTop() && mPoint.y < getBottom(); }
+			inline bool contains(const Vec2i& mPoint) const		{ return mPoint.x >= getLeft() && mPoint.x < getRight() && mPoint.y >= getTop() && mPoint.y < getBottom(); }
 	};
 }
 
