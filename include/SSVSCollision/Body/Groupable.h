0af37f0e9fa3d3f87a9fd93b1abfdadf1db2186a
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-10-09 23:38:11 +0200

    Refactorign

--- include/SSVSCollision/Body/Groupable.h
+++ include/SSVSCollision/Body/Groupable.h
@@ -17,18 +17,18 @@ namespace ssvsc
 		public:
 			inline void addGroups(Group mGroup)	noexcept													{ groups[mGroup] = true; }
 			inline void delGroups(Group mGroup)	noexcept													{ groups[mGroup] = false; }
-			template<typename... TGroups> inline void addGroups(Group mGroup, TGroups... mGroups) noexcept	{ groups[mGroup] = true; addGroups(mGroups...); }
-			template<typename... TGroups> inline void delGroups(Group mGroup, TGroups... mGroups) noexcept	{ groups[mGroup] = false; delGroups(mGroups...); }
+			template<typename... TGroups> inline void addGroups(Group mGroup, TGroups... mGroups) noexcept	{ addGroups(mGroup); addGroups(mGroups...); }
+			template<typename... TGroups> inline void delGroups(Group mGroup, TGroups... mGroups) noexcept	{ delGroups(mGroup); delGroups(mGroups...); }
 
 			inline void addGroupsToCheck(Group mGroup) noexcept														{ groupsToCheck[mGroup] = true; }
 			inline void delGroupsToCheck(Group mGroup) noexcept	 													{ groupsToCheck[mGroup] = false; }
-			template<typename... TGroups> inline void addGroupsToCheck(Group mGroup, TGroups... mGroups) noexcept	{ groupsToCheck[mGroup] = true; addGroupsToCheck(mGroups...); }
-			template<typename... TGroups> inline void delGroupsToCheck(Group mGroup, TGroups... mGroups) noexcept	{ groupsToCheck[mGroup] = false; delGroupsToCheck(mGroups...); }
+			template<typename... TGroups> inline void addGroupsToCheck(Group mGroup, TGroups... mGroups) noexcept	{ addGroupsToCheck(mGroup); addGroupsToCheck(mGroups...); }
+			template<typename... TGroups> inline void delGroupsToCheck(Group mGroup, TGroups... mGroups) noexcept	{ delGroupsToCheck(mGroup); delGroupsToCheck(mGroups...); }
 
 			inline void addGroupsNoResolve(Group mGroup) noexcept													{ groupsNoResolve[mGroup] = true; }
 			inline void delGroupsNoResolve(Group mGroup) noexcept													{ groupsNoResolve[mGroup] = false; }
-			template<typename... TGroups> inline void addGroupsNoResolve(Group mGroup, TGroups... mGroups) noexcept	{ groupsNoResolve[mGroup] = true; addGroupsNoResolve(mGroups...); }
-			template<typename... TGroups> inline void delGroupsNoResolve(Group mGroup, TGroups... mGroups) noexcept	{ groupsNoResolve[mGroup] = false; delGroupsNoResolve(mGroups...); }
+			template<typename... TGroups> inline void addGroupsNoResolve(Group mGroup, TGroups... mGroups) noexcept	{ addGroupsNoResolve(mGroup); addGroupsNoResolve(mGroups...); }
+			template<typename... TGroups> inline void delGroupsNoResolve(Group mGroup, TGroups... mGroups) noexcept	{ delGroupsNoResolve(mGroup); delGroupsNoResolve(mGroups...); }
 
 			inline void addGroup(Group mGroup) noexcept					{ addGroups(mGroup); }
 			inline void addGroupToCheck(Group mGroup) noexcept			{ addGroupsToCheck(mGroup); }
