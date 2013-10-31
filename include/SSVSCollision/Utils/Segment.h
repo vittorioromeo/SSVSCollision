dc394495053c871a9944dd48bc4070d17223090a
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-10-31 16:55:00 +0100

    Segment additions: > default constructor > getPointCrossProduct > isPointLeft/Right of this segment?

--- include/SSVSCollision/Utils/Segment.h
+++ include/SSVSCollision/Utils/Segment.h
@@ -12,7 +12,13 @@ namespace ssvsc
 	template<typename T> struct Segment
 	{
 		Vec2<T> start, end;
+
+		inline Segment() = default;
 		inline Segment(Vec2<T> mStart, Vec2<T> mEnd) noexcept : start{std::move(mStart)}, end{std::move(mEnd)} { }
+
+		inline T getPointCrossProduct(const Vec2<T>& mPoint) const noexcept	{ return ((end.x - start.x) * (mPoint.y - start.y) - (end.y - start.y) * (mPoint.x - start.x)); }
+		inline bool isPointLeft(const Vec2<T>& mPoint) const noexcept		{ return getPointCrossProduct(mPoint) > 0; }
+		inline bool isPointRight(const Vec2<T>& mPoint) const noexcept		{ return getPointCrossProduct(mPoint) < 0; }
 	};
 }
 
