f1213a55fc966aacafebb893325dccfe8b1102ea
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-02-12 00:31:36 +0100

    name refactoring

--- include/SSVSCollision/Body/Body.hpp
+++ include/SSVSCollision/Body/Body.hpp
@@ -29,30 +29,30 @@ namespace ssvsc
 			friend ResolverInfoType;
 
 		protected:
-			BodyData bodyData;
+			BodyData data;
 			std::vector<Body*> toResolve;
 			void* userData{nullptr};
 			bool mustInit{true};
 
 			inline void integrate(FT mFT) noexcept
 			{
-				bodyData.velocity += getAcceleration() * mFT;
+				data.velocity += getAcceleration() * mFT;
 				getShape().move(Vec2i(getVelocity() * mFT));
-				ssvs::nullify(bodyData.acceleration);
+				ssvs::nullify(data.acceleration);
 			}
 
 			inline void update(FT mFT)
 			{
 				if(mustInit) { this->spatialInfo.template init<BodyTag>(); mustInit = false; }
 
-				ssvs::nullify(bodyData.lastResolution);
+				ssvs::nullify(data.lastResolution);
 
 				this->onPreUpdate();
 
 				if(isStatic()) { this->spatialInfo.template preUpdate<BodyTag>(); return; }
 				if(this->outOfBounds) { onOutOfBounds(); this->outOfBounds = false; return; }
-				bodyData.oldShape = getShape();
-				bodyData.oldVelocity = getVelocity();
+				data.oldShape = getShape();
+				data.oldVelocity = getVelocity();
 				integrate(mFT);
 				this->spatialInfo.template preUpdate<BodyTag>();
 
@@ -81,55 +81,55 @@ namespace ssvsc
 			ssvu::Delegate<void()> onPostUpdate, onOutOfBounds;
 			ssvu::Delegate<void(const ResolutionInfoType&)> onResolution;
 
-			inline Body(TW& mWorld, bool mIsStatic, const Vec2i& mPos, const Vec2i& mSize) noexcept : Base<TW>{mWorld}, ResolverInfoType{*this}, bodyData{mIsStatic, mPos, mSize} { }
+			inline Body(TW& mWorld, bool mIsStatic, const Vec2i& mPos, const Vec2i& mSize) noexcept : Base<TW>{mWorld}, ResolverInfoType{*this}, data{mIsStatic, mPos, mSize} { }
 			inline ~Body() noexcept { destroy(); }
 			inline void destroy() { this->spatialInfo.template destroy<BodyTag>(); this->world.delBody(*this); }
 
-			inline void applyAccel(const Vec2f& mAccel) noexcept		{ bodyData.acceleration += mAccel; }
-			inline void resolvePosition(const Vec2i& mOffset) noexcept	{ bodyData.shape.move(mOffset); bodyData.lastResolution += mOffset; }
+			inline void applyAccel(const Vec2f& mAccel) noexcept		{ data.acceleration += mAccel; }
+			inline void resolvePosition(const Vec2i& mOffset) noexcept	{ data.shape.move(mOffset); data.lastResolution += mOffset; }
 
-			inline void setPosition(const Vec2i& mPos)					{ bodyData.oldShape = getShape(); bodyData.shape.setPosition(mPos);	this->spatialInfo.invalidate(); }
-			inline void setX(int mX)									{ bodyData.oldShape = getShape(); bodyData.shape.setX(mX);			this->spatialInfo.invalidate(); }
-			inline void setY(int mY)									{ bodyData.oldShape = getShape(); bodyData.shape.setY(mY);			this->spatialInfo.invalidate(); }
-			inline void setSize(const Vec2i& mSize)						{ bodyData.shape.setSize(mSize);									this->spatialInfo.invalidate(); }
-			inline void setHalfSize(const Vec2i& mSize)					{ bodyData.shape.setHalfSize(mSize);								this->spatialInfo.invalidate(); }
-			inline void setWidth(int mWidth)							{ bodyData.shape.setWidth(mWidth);									this->spatialInfo.invalidate(); }
-			inline void setHeight(int mHeight)							{ bodyData.shape.setHeight(mHeight);								this->spatialInfo.invalidate(); }
-			inline void setStatic(bool mStatic)							{ bodyData._static = mStatic;										this->spatialInfo.invalidate(); }
-			inline void setVelocity(const Vec2f& mVel) noexcept			{ bodyData.velocity = mVel; }
-			inline void setAcceleration(const Vec2f& mAccel) noexcept	{ bodyData.acceleration = mAccel; }
+			inline void setPosition(const Vec2i& mPos)					{ data.oldShape = getShape(); data.shape.setPosition(mPos);	this->spatialInfo.invalidate(); }
+			inline void setX(int mX)									{ data.oldShape = getShape(); data.shape.setX(mX);			this->spatialInfo.invalidate(); }
+			inline void setY(int mY)									{ data.oldShape = getShape(); data.shape.setY(mY);			this->spatialInfo.invalidate(); }
+			inline void setSize(const Vec2i& mSize)						{ data.shape.setSize(mSize);								this->spatialInfo.invalidate(); }
+			inline void setHalfSize(const Vec2i& mSize)					{ data.shape.setHalfSize(mSize);							this->spatialInfo.invalidate(); }
+			inline void setWidth(int mWidth)							{ data.shape.setWidth(mWidth);								this->spatialInfo.invalidate(); }
+			inline void setHeight(int mHeight)							{ data.shape.setHeight(mHeight);							this->spatialInfo.invalidate(); }
+			inline void setStatic(bool mStatic)							{ data._static = mStatic;									this->spatialInfo.invalidate(); }
+			inline void setVelocity(const Vec2f& mVel) noexcept			{ data.velocity = mVel; }
+			inline void setAcceleration(const Vec2f& mAccel) noexcept	{ data.acceleration = mAccel; }
 			inline void setUserData(void* mUserData) noexcept			{ userData = mUserData; }
-			inline void setVelocityX(float mX) noexcept					{ bodyData.velocity.x = mX; }
-			inline void setVelocityY(float mY) noexcept					{ bodyData.velocity.y = mY; }
-			inline void setResolve(bool mResolve) noexcept				{ bodyData.resolve = mResolve; }
-			inline void setMass(float mMass) noexcept					{ bodyData.setMass(mMass); }
-			inline void setRestitutionX(float mX) noexcept				{ bodyData.restitution.x = mX; }
-			inline void setRestitutionY(float mY) noexcept				{ bodyData.restitution.y = mY; }
+			inline void setVelocityX(float mX) noexcept					{ data.velocity.x = mX; }
+			inline void setVelocityY(float mY) noexcept					{ data.velocity.y = mY; }
+			inline void setResolve(bool mResolve) noexcept				{ data.resolve = mResolve; }
+			inline void setMass(float mMass) noexcept					{ data.setMass(mMass); }
+			inline void setRestitutionX(float mX) noexcept				{ data.restitution.x = mX; }
+			inline void setRestitutionY(float mY) noexcept				{ data.restitution.y = mY; }
 
-			inline AABB& getShape() noexcept							{ return bodyData.shape; }
-			inline AABB& getOldShape() noexcept							{ return bodyData.oldShape; }
-			inline const AABB& getShape() const noexcept				{ return bodyData.shape; }
-			inline const AABB& getOldShape() const noexcept				{ return bodyData.oldShape; }
+			inline AABB& getShape() noexcept							{ return data.shape; }
+			inline AABB& getOldShape() noexcept							{ return data.oldShape; }
+			inline const AABB& getShape() const noexcept				{ return data.shape; }
+			inline const AABB& getOldShape() const noexcept				{ return data.oldShape; }
 			inline const Vec2i& getPosition() const noexcept			{ return getShape().getPosition(); }
-			inline const Vec2f& getVelocity() const noexcept			{ return bodyData.velocity; }
+			inline const Vec2f& getVelocity() const noexcept			{ return data.velocity; }
 			inline const Vec2i& getOldPosition() const noexcept			{ return getOldShape().getPosition(); }
-			inline const Vec2f& getOldVelocity() const noexcept			{ return bodyData.oldVelocity; }
-			inline const Vec2f& getAcceleration() const noexcept		{ return bodyData.acceleration; }
+			inline const Vec2f& getOldVelocity() const noexcept			{ return data.oldVelocity; }
+			inline const Vec2f& getAcceleration() const noexcept		{ return data.acceleration; }
 			inline Vec2i getSize() const noexcept						{ return getShape().getSize(); }
-			inline float getMass() const noexcept						{ return isStatic() ? 0 : bodyData.mass; }
-			inline float getInvMass() const noexcept					{ return isStatic() ? 0 : bodyData.invMass; }
+			inline float getMass() const noexcept						{ return isStatic() ? 0 : data.mass; }
+			inline float getInvMass() const noexcept					{ return isStatic() ? 0 : data.invMass; }
 			inline int getWidth() const noexcept						{ return getShape().getWidth(); }
 			inline int getHeight() const noexcept						{ return getShape().getHeight(); }
-			inline bool isStatic() const noexcept						{ return bodyData._static; }
+			inline bool isStatic() const noexcept						{ return data._static; }
 			inline void* getUserData() const noexcept					{ return userData; }
 			inline bool hasMovedLeft() const noexcept					{ return getShape().getX() < getOldShape().getX(); }
 			inline bool hasMovedRight() const noexcept					{ return getShape().getX() > getOldShape().getX(); }
 			inline bool hasMovedUp() const noexcept						{ return getShape().getY() < getOldShape().getY(); }
 			inline bool hasMovedDown() const noexcept					{ return getShape().getY() > getOldShape().getY(); }
-			inline bool getResolve() const noexcept						{ return bodyData.resolve; }
-			inline const Vec2i& getLastResolution() const noexcept		{ return bodyData.lastResolution; }
-			inline float getRestitutionX() const noexcept				{ return bodyData.restitution.x; }
-			inline float getRestitutionY() const noexcept				{ return bodyData.restitution.y; }
+			inline bool getResolve() const noexcept						{ return data.resolve; }
+			inline const Vec2i& getLastResolution() const noexcept		{ return data.lastResolution; }
+			inline float getRestitutionX() const noexcept				{ return data.restitution.x; }
+			inline float getRestitutionY() const noexcept				{ return data.restitution.y; }
 
 			inline bool mustResolveAgainst(const Body& mBody) const noexcept { return getResolve() && !this->mustIgnoreResolution(mBody); }
 	};
